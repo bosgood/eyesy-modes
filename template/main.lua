3ac80f948c04c5979b95114862baf33f7b6c535a
Brad Osgood <bosgood@gmail.com>
2021-07-04 15:39:07 -0400

    Template updates

--- template/main.lua
+++ template/main.lua
@@ -1,13 +1,15 @@
+-- New Mode
+
 require("eyesy")
 
 NUM_SAMPLES = 256
-w = of.getWidth()
-h = of.getHeight()
-w2 = w / 2
-h2 = h / 2
-w4 = w / 4
-h4 = h / 4
-c = glm.vec3(w2, h2, 0)
+W = of.getWidth()
+H = of.getHeight()
+W2 = W / 2
+H2 = H / 2
+W4 = W / 4
+H4 = H / 4
+C = glm.vec3(W2, H2, 0)
 
 function setup()
   print("New Mode")
