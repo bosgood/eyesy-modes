476102c3563f25685587911562d4187f45770d90
Brad Osgood <bosgood@gmail.com>
2021-08-13 17:38:56 -0400

    Allow LowResNoiseScope mode to scale

--- modes/LowResNoiseScope/main.lua
+++ modes/LowResNoiseScope/main.lua
@@ -9,15 +9,8 @@ local compat = require("compat")
 -- knob4: 2d Perlin noise location
 
 NUM_SAMPLES = 256
-w = of.getWidth()
-h = of.getHeight()
-w2 = w / 2
-h2 = h / 2
-w4 = w / 4
-h4 = h / 4
-c = glm.vec3(w2, h2, 0)
-time = 0
 AMPLITUDE = 1500
+time = 0
 dir = 1
 
 function setup()
@@ -29,31 +22,31 @@ function update()
   compat:update()
 end
 
+local function drawSegment(x, y, width, scale, color, amp)
+  of.setColor(color)
+  of.beginShape()
+    local height = amp * scale
+    of.drawRectangle(x, y, width, height * 0.55)
+  of.endShape()
+end
+
 function draw()
   compat:draw()
-  scale = AMPLITUDE * knob1
+  local scale = AMPLITUDE * knob1
   time = (time + (dir * knob3)) % 255
   if time == 0 then
     dir = dir * -1
   end
-  barWidth = math.floor(w / NUM_SAMPLES)
+  local barWidth = math.floor(of.getWidth() / NUM_SAMPLES)
 
   for i=1,NUM_SAMPLES do
-    color = of.Color()
-    noise = of.noise(i, knob4)
+    local color = of.Color()
+    local noise = of.noise(i, knob4)
     color:setHsb(knob2 * 100 * noise, time, 255)
-    amp = inL[i]
+    local amp = inC[i]
     if i % 2 == 0 then
-      amp = inR[i]
+      amp = inC[i]
     end
-    drawSegment(i * barWidth, h2, barWidth, scale, color, amp)
+    drawSegment(i * barWidth, of.getHeight() / 2, barWidth, scale, color, amp)
   end
-end
-
-function drawSegment(x, y, width, scale, color, amp)
-  of.setColor(color)
-  of.beginShape()
-    height = amp * scale
-    of.drawRectangle(x, y, width, height * 0.55)
-  of.endShape()
 end
